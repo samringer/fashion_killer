847649e1919d1634781f71227d1998c43bf365bf
Sam Ringer <samr@speechmatics.com>
2019-10-10 08:25:44 +0100

    inc perc loss vgg in git

---
+++ app_transfer/model/perceptual_loss_vgg.py
@@ -0,0 +1,34 @@
+# Based on https://github.com/jantic/DeOldify/blob/master/fasterai/loss.py
+
+import torch
+from torch import nn
+from torchvision.models.vgg import vgg16
+
+
+class PerceptualLossVGG(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.vgg = vgg16(pretrained=True).features
+        blocks = [i-1 for i, o in enumerate(self.vgg.children())
+                  if isinstance(o, nn.MaxPool2d)]
+        self.layer_ids = blocks[2:5]
+        self.wgts = [20, 70, 10]
+
+    def get_features(self, x):
+        features = []
+        for i, block in enumerate(self.vgg):
+            x = block(x)
+            if i in self.layer_ids:
+                features.append(x)
+            if i >= self.layer_ids[-1]:
+                break
+        return features
+
+    def forward(self, x, target):
+        x_feats = self.get_features(x)
+        with torch.no_grad():
+            target_feats = self.get_features(target)
+        feat_losses = []
+        for x_feat, t_feat, wgt in zip(x_feats, target_feats, self.wgts):
+            feat_losses += [nn.L1Loss()(x_feat, t_feat)*wgt]
+        return sum(feat_losses)
